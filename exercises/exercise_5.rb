76b9197ef780e5c261922779cf4617adf1948ad4
bkcatton <ben.k.catton@gmail.com>
2022-03-08 11:59:04 -0800

    add a sum of annual revenue query, an average of annual revenue query, a count of stores making over M revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "total revenue for all stores $#{Store.sum(:annual_revenue)}"
+puts "average revenue for all stores $#{Store.average(:annual_revenue).to_i}"
+@number_of_mil_stores = Store.where("annual_revenue > ?", 1000000)
+puts "Number of stores generating over $1M in profit: #{@number_of_mil_stores.count}"
