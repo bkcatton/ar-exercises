2b10b568d8d5efb06de849394758d9594ec25871
bkcatton <ben.k.catton@gmail.com>
2022-03-08 12:33:01 -0800

    add employees to employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jim", last_name: "Bob", hourly_rate: 10)
+@store1.employees.create(first_name: "Tom", last_name: "Ron", hourly_rate: 50)
+@store1.employees.create(first_name: "Jon", last_name: "Don", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Tim", last_name: "Jim", hourly_rate: 60)
+@store2.employees.create(first_name: "Win", last_name: "Lim", hourly_rate: 10)
+@store2.employees.create(first_name: "Kim", last_name: "Pim", hourly_rate: 50)
+@store2.employees.create(first_name: "Jim", last_name: "Vim", hourly_rate: 100)
